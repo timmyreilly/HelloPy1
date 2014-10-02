e1bb15653ce9c6437c2929c46e1c5b92b1ce9537
timmytimreilly <timmytimreilly@outlook.com>
2014-10-02 13:30:42 -0700

    added dimensions to bmp

--- bmp.py
+++ bmp.py
@@ -72,3 +72,31 @@ def _int32_to_bytes(i):
                   i >> 8 & 0xff,
                   i >> 16 & 0xff,
                   i >> 24 & 0xff))
+
+def _bytes_to_int32(b):
+    '''Convert a bytes object containing four bytes into an integer.'''
+    return b[0] | (b[1] << 8) | (b[2] << 16) | (b[3] << 24)
+
+def dimensions(filename):
+    '''Determine the dimensions in pixels of a BMP image.
+    
+    Args:
+        filename: the filename of a BMP file.
+        
+    Returns:
+        A tuple containing two integers with the width and height in pixels
+        
+    Raises:
+        ValueError: If the file was not a BMP file.
+        OSError: If there was a problem reading the file. 
+    '''
+    with open(filename, 'rb') as f:
+        magic = f.read(2)
+        if magic != b'BM':
+            raise ValueError("{} is not a BMP file".format(filename))
+
+        f.seek(18)
+        width_bytes = f.read(4)
+        height_bytes = f.read(4)
+
+        return (_bytes_to_int32(width_bytes), _bytes_to_int32(height_bytes))
