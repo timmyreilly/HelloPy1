412d43854b9c1bf859fa49e41fd4eab9a95de2ba
timmytimreilly <timmytimreilly@outlook.com>
2014-10-03 11:12:25 -0700

    Added virtual environments

---
+++ foo/Scripts/easy_install-3.4-script.py
@@ -0,0 +1,10 @@
+#!C:\Users\tireilly\Documents\GitHub\PythonHello\helloPython\HelloPy1\HelloPy1\foo\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==6.0.2','console_scripts','easy_install-3.4'
+__requires__ = 'setuptools==6.0.2'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('setuptools==6.0.2', 'console_scripts', 'easy_install-3.4')()
+    )
